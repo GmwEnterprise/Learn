cf7a2d41bb3184c9c1ee306e9e3721dd34de33e2
GmwEnterprise <909498022@qq.com>
2019-04-29 17:36:04 +0800

    2019年4月29日 17:36:01

---
+++ SpringBoot集成Security/spring-security-learn/src/main/java/com/example/learn/security/SpringSecurityLearnApplication.java
@@ -0,0 +1,12 @@
+package com.example.learn.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLearnApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityLearnApplication.class, args);
+  }
+}
---
+++ 实战-SpringBoot企业级博客系统/blog-system/src/main/java/com/imooc/blog/BlogSystemApplication.java
@@ -0,0 +1,12 @@
+package com.imooc.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BlogSystemApplication.class, args);
+  }
+}
