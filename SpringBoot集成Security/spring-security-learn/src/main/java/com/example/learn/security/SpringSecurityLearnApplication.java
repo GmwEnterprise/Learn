dadadaeeed5814ee3b6976e197d413d412e407e3
GmwEnterprise <37724595+GmwEnterprise@users.noreply.github.com>
2019-05-03 16:42:13 +0800

    gmw

--- SpringBoot集成Security/spring-security-learn/src/main/java/com/example/learn/security/SpringSecurityLearnApplication.java
+++ SpringBoot集成Security/spring-security-learn/src/main/java/com/example/learn/security/SpringSecurityLearnApplication.java
@@ -3,6 +3,9 @@ package com.example.learn.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Gmw
+ */
 @SpringBootApplication
 public class SpringSecurityLearnApplication {
 
