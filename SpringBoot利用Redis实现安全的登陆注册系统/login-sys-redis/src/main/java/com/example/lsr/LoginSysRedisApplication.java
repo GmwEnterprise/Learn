9bcaca7f012586ea1b0c1515ae2eb8dbcfe5b555
GmwEnterprise <37724595+GmwEnterprise@users.noreply.github.com>
2019-05-07 17:40:38 +0800

    2019年5月7日 17:40:27

---
+++ SpringBoot利用Redis实现安全的登陆注册系统/login-sys-redis/src/main/java/com/example/lsr/LoginSysRedisApplication.java
@@ -0,0 +1,12 @@
+package com.example.lsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSysRedisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoginSysRedisApplication.class, args);
+  }
+}
---
+++ SpringBoot利用Redis实现安全的登陆注册系统/login-sys-redis/src/test/java/com/example/loginsysredis/LoginSysRedisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.loginsysredis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginSysRedisApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
