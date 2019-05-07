dedb8944d7f61cee85eb0f412d1eb41afdce77c0
GmwEnterprise <909498022@qq.com>
2019-05-08 00:16:50 +0800

    2019年5月8日 00:16:48

---
+++ SpringBoot集成JPA/jpa-hibernate/src/main/java/com/example/jpahibernate/JpaHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.example.jpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpaHibernateApplication.class, args);
+  }
+}
---
+++ SpringBoot集成JPA/jpa-hibernate/src/test/java/com/example/jpahibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jpahibernate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaHibernateApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
