d25a77a555883175ef6b9a120f2ce6aa3b3ade2c
gmw mac os <gmw@gmwdeiMac.local>
2019-07-14 23:14:47 +0800

    gmw

--- presevere-vue/src/components/common/LoginForm.vue
+++ presevere-vue/src/components/common/LoginForm.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>LoginForm</div>
+  <div>
+    <b-form @submit="submit()">
+      <b-form-group>
+        <b-form-input id="input-phone" v-model="phone" required placeholder="输入你的手机号码"></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary" style="float: right;">确定</b-button>
+    </b-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginForm'
+  name: 'LoginForm',
+  data() {
+    return {
+      phone: ''
+    }
+  },
+  methods: {
+    submit() {}
+  }
 }
 </script>
 
