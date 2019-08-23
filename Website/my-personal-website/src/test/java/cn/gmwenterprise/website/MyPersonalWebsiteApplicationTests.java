45cdfc69aef076f996936deccbbe96888b8b7393
GmwEnterprise <909498022@qq.com>
2019-08-23 18:28:31 +0800

    gmw

--- Website/my-personal-website/src/test/java/cn/gmwenterprise/website/MyPersonalWebsiteApplicationTests.java
+++ Website/my-personal-website/src/test/java/cn/gmwenterprise/website/MyPersonalWebsiteApplicationTests.java
@@ -1,23 +1,13 @@
 package cn.gmwenterprise.website;
 
-import cn.gmwenterprise.website.config.mybatis.Page;
-import cn.gmwenterprise.website.config.mybatis.PageHelper;
-import cn.gmwenterprise.website.dao.AccountDao;
-import cn.gmwenterprise.website.domain.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
-import java.util.List;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class MyPersonalWebsiteApplicationTests {
-
-    @Resource
-    AccountDao accountDao;
 
     @Test
     public void contextLoads() {
