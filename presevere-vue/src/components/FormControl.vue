83ad6c0416fad404109615d3fb45ad4503f94982
GmwEnterprise <909498022@qq.com>
2019-09-03 17:42:40 +0800

    gmw

--- presevere-vue/src/components/FormControl.vue
+++ presevere-vue/src/components/FormControl.vue
@@ -1,16 +1,51 @@
 <template>
-  <div class="form-control"></div>
+  <div class="form-control">
+    <template v-if="type === 'number'">
+      
+    </template>
+    <template v-else-if="type === 'date-range'">
+      
+    </template>
+    <template v-else-if="type === 'single-select'">
+      
+    </template>
+    <template v-else-if="type === 'multipart-select'">
+      
+    </template>
+    <template v-else>
+      <label for=""></label>
+      <input type="text" :value="value" @input="emitEvent">
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'QueryFormControlItem',
   props: {
+    // 确定输入框的类型, 有text/number/date-range/single-select/multipart-select
+    type: {
+      type: String,
+      required: false,
+      default: 'text'
+    },
+    // label 标签
+    label: {
+      type: String,
+      required: true
+    },
+    // value 绑定的值，类型为any
+    value: {
+      required: true
+    }
+  },
+  methods: {
     /**
-     * 确定输入框的类型, 有text/number/date-range/single-select/multipart-select
+     * @param {Event} e
      */
-    type: String,
-
+    emitEvent(e) {
+      this.$emit('input', e.target.value)
+    }
   }
 }
 </script>
