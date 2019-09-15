d4c61e91cd3cec5daa6cc7e7ea79b4606056efff
GmwEnterprise <909498022@qq.com>
2019-09-15 23:33:27 +0800

    gmw

--- dubbo/dubbo.apache.org/dubbo-start/client/src/main/java/cn/gmwenterprise/ClientApplication.java
+++ dubbo/dubbo.apache.org/dubbo-start/client/src/main/java/cn/gmwenterprise/ClientApplication.java
@@ -2,10 +2,8 @@ package cn.gmwenterprise;
 
 import cn.gmwenterprise.service.EchoService;
 import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
